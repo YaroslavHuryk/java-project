c1d7bfcd7a903138dbcca72438c5c3c7a897bbb5
YaroslavHuryk <yaroslavhuryk63@gmail.com>
2024-06-07 10:32:57 +0300

    Added some changes in test

--- demo/src/test/java/com/practice/demo/controller/CustomerControllerTest.java
+++ demo/src/test/java/com/practice/demo/controller/CustomerControllerTest.java
@@ -1,23 +1,19 @@
 package com.practice.demo.controller;
 
-import com.practice.demo.DemoApplication;
 import com.practice.demo.repository.CustomerRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = DemoApplication.class)
 @WebMvcTest(CustomerController.class)
 public class CustomerControllerTest {
 
@@ -26,9 +22,6 @@ public class CustomerControllerTest {
 
     @MockBean
     private CustomerRepo customerRepo;
-
-    @InjectMocks
-    private CustomerController customerController;
 
     @BeforeEach
     public void setUp() {
